8e06665288133834e91b0f1f469368e9c7cf8397
Michelleesquivel <40812951+Michelleesquivel@users.noreply.github.com>
2018-07-08 21:30:51 -0600

    Add files via upload

---
+++ Radio.kt
@@ -0,0 +1,11 @@
+// Michelle Esquivel
+// 08.07.2018
+// Radio, programa que indica la funcionabilidad de un radio y su clase
+
+// Clase radio
+
+class Radio (
+        val emision: Int,
+        val volume: String,
+        val isTurnedOn: Boolean = false,
+        val estacion: Int)
